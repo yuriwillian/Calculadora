139b4d153c6f6e87ee546b80b0ccb85bea90a1db
Yuri Willian <yuriwilian@gmail.com>
2022-01-03 17:08:47 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,19 @@
+function insert(num) {
+    document.getElementById("resultado").innerHTML += num
+}
+
+function limpar(){
+    document.getElementById("resultado").innerHTML = ""
+}
+
+function backspace(){
+    var resultado = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular(){
+    var resultado = document.getElementById("resultado").innerHTML
+    if(resultado){
+        document.getElementById("resultado").innerHTML = eval(resultado) // "eval" calcula todas as operações matemáticas em um comando!!!!!!
+    }
+}
